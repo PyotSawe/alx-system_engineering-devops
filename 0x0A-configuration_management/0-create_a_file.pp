04c2c1dff9f53b8ba94f94d0b748febf5ced5b96
PyotSawe <potsaw01@gmail.com>
2023-11-25 04:36:00 +0300

    Fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# Create a file in /tmp
+# Create a file using Puppet
 
-file { 'school':
+file { '/tmp/holberton':
   ensure  => 'present',
   content => 'I love Puppet',
-  group   => 'www-data',
-  mode    => '0744',
   owner   => 'www-data',
-  path    => '/tmp/school,
+  group   => 'www-data',
+  mode    => '0744'
 }
